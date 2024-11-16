84f21c22b7fe2803f765d8531ccab767b6216261
Viktor Shevchuk <viktor.shevchuk.dev@gmail.com>
2024-11-16 12:28:30 +0100

    init

--- src/App.js
+++ src/App.js
@@ -1,31 +1,8 @@
 import { Component } from 'react';
 
-import Player from './components/Player/Player.component';
-import VideoList from './components/VideoList/VideoList.component';
-import Reader from './components/Reader/Reader.component';
-
-import videos from './videos.json';
-import publications from './publications.json';
-
 class App extends Component {
-  state = { selectedVideo: null };
-
-  selectVideo = (link) => {
-    this.setState({ selectedVideo: link });
-  };
-
   render() {
-    return (
-      <>
-        <div style={{ padding: 24 }}>
-          <h1>Selected video: {this.state.selectedVideo}</h1>
-          <VideoList videos={videos} onSelect={this.selectVideo} />
-          <Player url={this.state.selectedVideo} />
-        </div>
-
-        <Reader publications={publications} />
-      </>
-    );
+    return <></>;
   }
 }
 
